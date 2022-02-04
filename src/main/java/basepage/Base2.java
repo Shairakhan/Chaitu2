704d3284df8efe99c03116970dbed718405ebe00
AijazDivecha <aijaz.divecha@gmail.com>
2022-02-04 22:27:06 +0000

    Created a base file in base package

--- src/main/java/basepage/Base2.java
+++ src/main/java/basepage/Base2.java
@@ -2,7 +2,8 @@ package basepage;
 
 public class Base2
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("hi");
     }
 }
