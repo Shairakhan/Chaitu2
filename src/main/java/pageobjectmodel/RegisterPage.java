936f6e3dc67b1eec423f435c8d2e3cbb2695e38f
AijazDivecha <aijaz.divecha@gmail.com>
2022-02-04 23:01:38 +0000

    Aijaz here created a new Class in pageobject package

---
+++ src/main/java/pageobjectmodel/RegisterPage.java
@@ -0,0 +1,8 @@
+package pageobjectmodel;
+
+public class RegisterPage
+{
+    public static void main(String[] args) {
+        System.out.println("Hi shaira I have created a register class regards aijaz");
+    }
+}
